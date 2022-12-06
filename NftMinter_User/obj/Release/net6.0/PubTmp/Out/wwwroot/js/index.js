897dc643fdea454fb162e2839f72b7d9ece2b44e
강범수 <kbs21c1987@gamil.com>
2022-12-06 19:00:42 +0900

    minter obj vin 폴더 삭제

--- NftMinter_User/obj/Release/net6.0/PubTmp/Out/wwwroot/js/index.js
+++
@@ -1,9 +0,0 @@
-﻿const emailInput = document.querySelector('.email_input');
-const emailSendBtn = document.querySelector('.submit_btn');
-
-const ab = "asdasdasd"
-
-
-emailSendBtn.addEventListener('click', () => {
-    EmailRegExp(emailInput);
-})

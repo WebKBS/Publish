0b312940ccf340576643a2dcce47a76528854e9f
강범수 <kbs21c1987@gamil.com>
2022-12-20 18:58:30 +0900

    20221220 테스트중

--- Airdropper/wwwroot/js/windowCheck.js
+++ Airdropper/wwwroot/js/windowCheck.js
@@ -1,13 +1,52 @@
 ﻿window.addEventListener('resize', () => {
+
     windowSizeCheck();
 })
 windowSizeCheck();
 
 function windowSizeCheck() {
+
+    const elementMsg = `<div id="windowCheckErrorMsg" style="text-align: center; padding: 100px 24px; word-break: keep-all">1024p이하 해상도 및 모바일에서는 접속 하실수 없습니다.
+                <br>
+                PC를 이용해주시길 바랍니다.
+                <br>
+                <span class="timer">10</span>초뒤 이전페이지로 이동합니다.
+                <br><br>
+                1024p or lower resolution and cannot be accessed on mobile devices.
+                <br>
+                Please use a PC.
+                <br>
+                Go to the previous page after <span class="timer">10</span> seconds.
+                </div>`
+
+
     if (window.innerWidth < 1024 || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        document.body.style.display = "none";
+        if (document.getElementById("wrap").style.display === "none") {
+            return;
+        }
+
+        document.getElementById("wrap").style.display = "none";
         alert('1024p이하 해상도 및 모바일에서는 접속 하실수 없습니다.\nPC를 이용해주시길 바랍니다.');
+        document.body.insertAdjacentHTML('beforeend', elementMsg);
+
+        document.querySelectorAll('.timer').forEach(ev => {
+            let num = 10;
+
+            const timer = setInterval(() => {
+                num -= 1;
+                ev.textContent = num;
+                if (num === 0) {
+                    window.location.href = history.back();
+                    clearInterval(timer);
+                }
+            }, 1000)
+        })
+
     } else {
-        document.body.style.display = "block";
+        document.getElementById("wrap").style.display = "block";
+        if (document.getElementById('windowCheckErrorMsg')) {
+            document.getElementById('windowCheckErrorMsg').remove();
+            return;
+        }
     }
 }

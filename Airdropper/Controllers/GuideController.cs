caf8c4e56286c3efa8d72b9a80ef3c63725a4a06
강범수 <kbs21c1987@gamil.com>
2022-12-23 15:29:07 +0900

    가이드 추가 완료

---
+++ Airdropper/Controllers/GuideController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Airdropper.Controllers
+{
+    public class GuideController : Controller
+    {
+        public IActionResult UseGuide()
+        {
+            return View();
+        }
+        public IActionResult PriceGuide()
+        {
+            return View();
+        }
+    }
+}

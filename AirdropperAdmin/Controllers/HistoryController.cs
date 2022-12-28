2573229743c72dddb4484694b448d036461c6684
강범수 <kbs21c1987@gamil.com>
2022-12-28 18:56:36 +0900

    어드민 제작중

---
+++ AirdropperAdmin/Controllers/HistoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirdropperAdmin.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
---
+++ AirdropperAdmin/Controllers/NoticeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirdropperAdmin.Controllers
+{
+    public class NoticeController : Controller
+    {
+        public IActionResult Notice()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}

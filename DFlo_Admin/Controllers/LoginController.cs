36c5f8422a10c32f5b1dbdbea43fbac2ccfc3ee1
강범수 <kbs21c1987@gamil.com>
2023-01-05 18:40:09 +0900

    Dflo 관리자 작업

---
+++ DFlo_Admin/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DFlo_Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
